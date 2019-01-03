5782b2fc748806e6ab1e8d9b721fb04967563c8d
Jhonbeltran <jbeltranleon@gmail.com>
2019-01-03 11:47:29 -0500

    create de include urls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} by @'.format(self.question, self.created_by)
+
+
+class Choice(models.Model):
+    # The name to use for the relation from the related object 
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        # Sets of field names that, taken together, must be unique
+        unique_together = ("poll", "voted_by")
