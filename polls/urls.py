81b06d44778255d6b1f026c2be2ed7addab8fdea
Jhonbeltran <jbeltranleon@gmail.com>
2019-01-03 12:09:01 -0500

    agrego las vistas base y urls para una api rest con django puro: list: lista de encuestas y encuestas individuales

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from views import polls_list, polls_detail
 
 urlpatterns = [
-    
+    path("polls/", polls_list, name="list"),
+    path("polls/<int:pk>/", polls_detail, name="detail")
 ]
