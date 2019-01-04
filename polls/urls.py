d448be553863c35c04ec655fb3eb14537e48e775
Jhonbeltran <jbeltranleon@gmail.com>
2019-01-04 10:27:42 -0500

    Uso apiview para crear las vistas

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import polls_list, polls_detail
+from .apiviews import PollList, PollDetail
 
 urlpatterns = [
-    path("polls/", polls_list, name="list"),
-    path("polls/<int:pk>/", polls_detail, name="detail")
+    path("polls/", PollList.as_view(), name="list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="detail")
 ]
